51006cc7a549cd344ca99c20493d0a1c39c30742
Usama Sidat <97623263+usama-s786@users.noreply.github.com>
2022-06-23 14:52:01 -0400

    Add files via upload

---
+++ Final_Project/Version03/Main_Module/main.c
@@ -0,0 +1,12 @@
+#include "fundamentals.h"       //Including the fundamentals header file
+#include "manipulating.h"       //Including the manipulating header file
+#include "converting.h"         //Including the converting header file
+#include "tokenizing.h"        //Including the tokenizing header file
+
+int main() {
+	fundamentals();         //fundamentals function call
+	anipulating();         //manipulating function call
+	converting();           //converting function call
+	tokenizing();           //tokenizing function call
+	return 0;               //returning control to the OS
+}
